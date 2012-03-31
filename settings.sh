5f3f07a5abf0f434df37a2fb572eb101585b67b5
Anthony Feick <afeick@gmail.com>
2012-03-31 16:59:59 -0500

    add new settings and make comments echo

--- settings.sh
+++ settings.sh
@@ -3,73 +3,85 @@
 # http://knoopx.net/2011/10/28/os-x-lion-tweaks
 
 
-# Show all filename extensions in Finder
+echo "Show all filename extensions in Finder"
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-# Expand save panel by default
+echo "Use current directory as default search scope in Finder"
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+echo "Show Path bar in Finder"
+defaults write com.apple.finder ShowPathbar -bool true
+
+echo "Show Status bar in Finder"
+defaults write com.apple.finder ShowStatusBar -bool true
+
+echo "Expand save panel by default"
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 
-# Disable the “Are you sure you want to open this application?” dialog
+echo "Expand print panel by default"
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+
+#echo "Disable the “Are you sure you want to open this application?” dialog"
 #defaults write com.apple.LaunchServices LSQuarantine -bool false
 
-# Disable the warning when changing a file extension
+echo "Disable the warning when changing a file extension"
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
-# Disable shadow in screenshots
+#echo "Disable shadow in screenshots"
 #defaults write com.apple.screencapture disable-shadow -bool true
 
-# Enable highlight hover effect for the grid view of a stack (Dock)
+echo "Enable highlight hover effect for the grid view of a stack (Dock)"
 defaults write com.apple.dock mouse-over-hilte-stack -bool true
 
-# Enable spring loading for all Dock items
+echo "Enable spring loading for all Dock items"
 defaults write enable-spring-load-actions-on-all-items -bool true
 
-# Show indicator lights for open applications in the Dock
+#echo "Show indicator lights for open applications in the Dock"
 #defaults write com.apple.dock show-process-indicators -bool true
 
-# Don’t animate opening applications from the Dock
+#echo "Don’t animate opening applications from the Dock"
 #defaults write com.apple.dock launchanim -bool false
 
-# Automatically open a new Finder window when a volume is mounted
+echo "Automatically open a new Finder window when a volume is mounted"
 defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
 defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
 defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
 
-# Display full POSIX path as Finder window title
+echo "Display full POSIX path as Finder window title"
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
-# Enable the 2D Dock
+#echo "Enable the 2D Dock"
 #defaults write com.apple.dock no-glass -bool true
 
-# Show indicator lights for open applications in the Dock
+echo "Show indicator lights for open applications in the Dock"
 defaults write com.apple.dock show-process-indicators -bool true
 
-# Make Dock icons of hidden applications translucent
+echo "Make Dock icons of hidden applications translucent"
 defaults write com.apple.dock showhidden -bool true
 
-# Enable iTunes track notifications in the Dock
+echo "Enable iTunes track notifications in the Dock"
 defaults write com.apple.dock itunes-notifications -bool true
 
-# Avoid creating .DS_Store files on network volumes
+echo "Avoid creating .DS_Store files on network volumes"
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
-# Only use UTF-8 in Terminal.app
+echo "Only use UTF-8 in Terminal.app"
 defaults write com.apple.terminal StringEncodings -array 4
 
-# Disable the Ping sidebar in iTunes
+echo "Disable the Ping sidebar in iTunes"
 defaults write com.apple.iTunes disablePingSidebar -bool true
 
-# Disable all the other Ping stuff in iTunes
+echo "Disable all the other Ping stuff in iTunes"
 defaults write com.apple.iTunes disablePing -bool true
 
-# Make ⌘ + F focus the search input in iTunes
+echo "Make ⌘ + F focus the search input in iTunes"
 defaults write com.apple.iTunes NSUserKeyEquivalents -dict-add "Target Search Field" "@F"
 
-# Show the ~/Library folder
+echo "Show the ~/Library folder"
 chflags nohidden ~/Library
 
-# Kill affected applications
+echo "Kill affected applications"
 for app in Safari Finder Dock Mail; do killall "$app"; done
 
-# Fix for the ancient UTF-8 bug in QuickLook (http://mths.be/bbo)
+echo "Fix for the ancient UTF-8 bug in QuickLook (http://mths.be/bbo)"
 echo "0x08000100:0" > ~/.CFUserTextEncoding
